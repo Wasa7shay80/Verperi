600fb91dfe38001ec5d08c71984f69e57740f104
Wasa7shay80 <158795096+Wasa7shay80@users.noreply.github.com>
2025-08-15 06:25:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7040944963"))
 PORT = os.environ.get("PORT", "1311")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Whigga:Whigga80@whigga.9q2j0.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Veriperi:akshay8080@veriperi.j6svxao.mongodb.net/?retryWrites=true&w=majority")
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DB_URI)
 DB_NAME = os.environ.get("DATABASE_NAME", "Snappyy80")
 
